ffd030ae192d97eff380a56b98062668b55ee275
Ryan <chenryan11@yahoo.com.tw>
2019-11-07 00:46:30 +0800

    first

---
+++ HanoiGameMain/HanoiGameMain/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HanoiGameMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HanoiGame game = new HanoiGame();
+            game.Setup();
+            game.Play();
+
+            Console.ReadKey();
+        }
+    }
+}
